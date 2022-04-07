e3f80c21b3ef10f41605d0dc471feb11c2b0e4a9
noraalobaidi <nalobaidi_@hotmail.com>
2022-04-07 03:11:10 +0300

    MODIFIED files

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//uncompleted :taskChecked &filterTasks
 // You do not need to change the constants
 const CATEGORY_SELECTOR = "categories-list";
 const CATEGORY_FILTER = "categories-list-filter";
 
 let tasks = [];
 let categories = [];
+let doneTasks = [];
 
 // REMOVE ME: SAMPLE FILLING
 tasks = [
@@ -13,13 +15,23 @@ tasks = [
 
 categories = ["Movies", "Groceries"];
 // SAMPLE
-renderCategories(categories, CATEGORY_SELECTOR);
-renderCategories(categories, CATEGORY_FILTER);
-renderTasks(tasks, "tasks-list");
+// renderCategories(categories, CATEGORY_SELECTOR);
+// renderCategories(categories, CATEGORY_FILTER);
+// renderTasks(tasks, "tasks-list");
 
 function taskChecked(taskId, checked) {
   // implement the delete task.
   // You are given the task id
+  if (checked) {
+    tasks.forEach((task) => {
+      if (task[id] === taskId) {
+        let index = indexOf(task);
+      }
+    });
+    let deletedTask = tasks.splice(index, 1); //removing the task from tasks original list
+    doneTasks.push(deletedTask); //saving a copy
+  }
+  renderTasks(tasks, "tasks-list");
   console.log(`${checked ? "" : "UN"}CHECKED TASK`, taskId);
 }
 
@@ -27,12 +39,24 @@ function addTask() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
   const taskTitle = getNewTaskText();
   // continue the code here
+  let idnum = tasks.length; ///
+  let taskObj = {
+    id: idnum,
+    title: taskTitle,
+    category: selectedCategory,
+    done: false,
+  };
+  tasks.push(taskObj); ///
+  renderTasks(tasks, "tasks-list"); ///
   alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
 }
 
 function addCategory() {
   const newCategory = getNewCategoryText();
   // continue the code here
+  categories.push(newCategory); ///
+  renderCategories(categories, CATEGORY_SELECTOR);
+  renderCategories(categories, CATEGORY_FILTER);
   alert(`New category was added: ${newCategory}`);
 }
 
@@ -40,6 +64,9 @@ function filterTasks() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_FILTER);
   const done = getFilteredDone();
   // continue the code here
+  let filtertasks = tasks.filter((task) => task[category] === selectedCategory);
+  renderTasks(filtertasks); ///
+
   // REMOVE ME: sample alert
   alert(`Category: ${selectedCategory} | done: ${done}`);
 }
